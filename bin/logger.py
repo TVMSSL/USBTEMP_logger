00714879ac2c4b3b7cdbb6160bba70f1a0241b76
TheoBDV <t.villiers@ucl.ac.uk>
2021-01-28 15:55:49 +0000

    EM TVAC ready

--- bin/logger.py
+++ bin/logger.py
@@ -153,7 +153,7 @@ class ULTI02(UIExample):
                 print('Test Running...')
                 print('Test Name =', self.test_name)
                 print('Last logged data:', self.last_logged_time)
-                for x in range(0, 7):
+                for x in range(0, 8):
                     print('Channel', x, 'T=', round(interp_resist_to_temp_np1000(data_temp_display[x]),2), '°C')
                 self.verify = 5
             elif message_timing > 4 and self.verify == 5:
